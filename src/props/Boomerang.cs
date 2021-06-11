db8ff3136b2d89d918b32b31935cf1f02a8081ad
Craig Post <cpost@bitbuckets.org>
2021-06-10 21:07:52 -0700

    Added non-boomerang boomerang throw
    for testing

--- src/props/Boomerang.cs
+++ src/props/Boomerang.cs
@@ -3,20 +3,62 @@ using System;
 
 public class Boomerang : KinematicBody2D
 {
+    const float Speed = 100f;
 
-    // Called when the node enters the scene tree for the first time.
+    Vector2 Direction { get; set; }
+    float Force { get; set; }
+    bool Moving { get; set; }
+
+    Node parent;
+
+    VisibilityNotifier2D visibilityNotifier2D;
     public override void _Ready()
     {
+        visibilityNotifier2D = GetNode<VisibilityNotifier2D>("VisibilityNotifier2D");
+        parent = GetParent();
+    }
+
+    public override void _PhysicsProcess(float delta)
+    {
+        base._PhysicsProcess(delta);
+
+        if (Moving)
+        {
+            Position += Direction * delta * Speed * Force;
+        }
+
+        if (!visibilityNotifier2D.IsOnScreen())
+        {
+            Moving = false;
+            Position = Vector2.Zero;
+            if (parent != null && GetParent() != null)
+            {
+                // remove ourselves from whoever we are attached to and add us back to
+                // our original parent.
+                GetParent().RemoveChild(this);
+                parent?.AddChild(this);
+            }
+        }
 
     }
 
     // Scott: please give this a proper name and proper parameters i just put this here to have the equation in the codebase somewhere
-    public double equation(double direction, double force)
+    public void Throw(double angle, float force)
     {
+        Moving = true;
+        Direction = new Vector2((float)Math.Sin(angle), -(float)Math.Cos(angle)).Normalized();
+        Force = force;
 
-        GD.Print("LAUNCHED AT " + force + "!!!");
+        // remove ourselves from the Player node so we don't rotate or move with it
+        // while being thrown
+        // then add ourselves to the root scene tree
+        var globalPosition = GlobalPosition;
+        var root = GetTree().Root;
+        parent?.RemoveChild(this);
+        root.AddChild(this);
+        GlobalPosition = globalPosition;
 
-        return 0;
+        GD.Print($"LAUNCHED AT force: {force}, direction: {Direction} !!!");
     }
 
 }
