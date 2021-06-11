239285bb49cbdd58d61ce66c3f258c379809787b
Ojas Sanghi <ojas.sanghi@protonmail.com>
2021-06-10 18:17:41 -0700

    And collision and collection-detection for Boomerangs and Gems

--- src/singletons/Events.cs
+++ src/singletons/Events.cs
@@ -6,9 +6,12 @@ public class Events : Node
 
     public static event Action<int> levelCompleted;
     public static event Action nextLevelPressed;
+    public static event Action gemCollected;
+
 
     ////////////////////////////////////
 
     public static void publishLevelCompeted(int stars) => levelCompleted?.Invoke(stars);
     public static void publishNextLevelPressed() => nextLevelPressed?.Invoke();
+    public static void publishGemCollected() => gemCollected?.Invoke();
 }
