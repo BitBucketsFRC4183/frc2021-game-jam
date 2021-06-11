44e7bcdbbd8829a1e3618fca21d64701bdb3e894
Craig Post <cpost@bitbuckets.org>
2021-06-10 20:39:50 -0700

    Added HUDContainer for instructions
    Moved Boomerang under PlayerSprites node

--- src/levels/BaseLevel.cs
+++ src/levels/BaseLevel.cs
@@ -7,11 +7,14 @@ public class BaseLevel : Node2D
 
     Menu Menu { get; set; }
     Button TempNextLvlButton { get; set; }
+    Control HUDContainer { get; set; }
 
     public override void _Ready()
     {
         Menu = GetNode<Menu>("Menu");
+        HUDContainer = (Control)FindNode("HUDContainer");
         TempNextLvlButton = (Button)FindNode("TempNextLvlButton");
+
         TempNextLvlButton.Connect("pressed", this, nameof(OnTempNextLvlButtonPressed));
 
         if (!LevelsInfo.Instance.gameStarted)
@@ -32,6 +35,7 @@ public class BaseLevel : Node2D
         Menu.Visible = false;
         GetTree().Paused = false;
         TempNextLvlButton.Visible = true;
+        HUDContainer.Visible = true;
     }
 
     public override void _UnhandledInput(InputEvent @event)
@@ -48,6 +52,7 @@ public class BaseLevel : Node2D
         // show the menu and pause the game
         Menu.Visible = true;
         GetTree().Paused = true;
+        HUDContainer.Visible = false;
     }
 
     void OnTempNextLvlButtonPressed()
